79185a30bfdbff615a71235e9cd0c2f87ab601c2
Conner <conmanmojo1@gmail.com>
2021-03-26 14:08:32 -0500

    Adding everything from 'flaskmake'

---
+++ meowsite/appserver.py
@@ -0,0 +1,21 @@
+# Don't call this flask.py!
+# Documentation for Flask can be found at:
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os 
+
+app = Flask(__name__)
+app.secret_key = b'REPLACE_ME_x#pi*CO0@^z'
+
+sqlite_uri = 'sqlite:///' + os.path.abspath(os.path.curdir) + '/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import User
+
+@app.route('/')
+def index():
+    return 'Results of GET /'
